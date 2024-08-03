6a188091c25bdfb0868ccc4eb365d0649f76a5f1
evilat <gameonhayby@gmail.com>
2024-08-04 03:20:41 +0500

    Basic views.py

--- website/views.py
+++ website/views.py
@@ -1,8 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from models import Product, Sale, StockMovement
 from . import db
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return  render_template("home.html")
+
+    products = Product.all()
+
+    return  render_template("home.html", products=products)
+
+@views.route('/create_product')
+def create_product():
+    product = request.form.get('product')
+
+    return render_template("products.html", product=product)

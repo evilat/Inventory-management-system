d61d65f4885568b69eea9b26bcad4cb4bdd5b7c7
evilat <gameonhayby@gmail.com>
2024-08-03 20:26:13 +0500

    Locally hosted

--- website/__init__.py
+++ website/__init__.py
@@ -9,3 +9,11 @@ def create_app():
     app.secret_key = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
